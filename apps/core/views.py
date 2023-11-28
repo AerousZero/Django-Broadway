f91523e981ced1455e4a26f90af659113af9e30b
AerousZero <srijanshakya77@gmail.com>
2023-11-28 14:45:29 +0545

    Apps and Static File for user login

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from apps.core.models import Job, JobApplication, Category
 
 # Create your views here.
+class HomeView(ListView):
+    template_name = "core/home.html"
+    queryset = Job.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = "Home"
+        return context
+
+
